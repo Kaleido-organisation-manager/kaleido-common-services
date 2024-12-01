5163f4ee4e6ef736d6feabe66e530c6fb2eb9114
Dries Delanghe <75736368+DriesDelanghe@users.noreply.github.com>
2024-12-01 01:05:14 +0100

    fixed invalid managerResponse object (#25)
    Co-authored-by: Dries Delanghe <dries.delanghe@nara.to>

--- src/Kaleido.Common.Services.Grpc/Models/ManagerResponse.cs
+++ src/Kaleido.Common.Services.Grpc/Models/ManagerResponse.cs
@@ -2,10 +2,15 @@ using Kaleido.Common.Services.Grpc.Constants;
 
 namespace Kaleido.Common.Services.Grpc.Models;
 
-public readonly struct ManagerResponse<T> where T : class
+public class ManagerResponse<T> where T : class
 {
-    public readonly ManagerResponseState State { get; }
-    public readonly T? Result { get; }
+    public readonly ManagerResponseState State;
+    public readonly T? Result;
+
+    public ManagerResponse()
+    {
+        State = ManagerResponseState.Unknown;
+    }
 
     private ManagerResponse(ManagerResponseState state, T? result = default)
     {
@@ -18,11 +23,16 @@ public readonly struct ManagerResponse<T> where T : class
     public static ManagerResponse<T> NotModified() => new(ManagerResponseState.NotModified);
 }
 
-public readonly struct ManagerResponse<T1, T2> where T1 : class where T2 : class
+public class ManagerResponse<T1, T2> where T1 : class where T2 : class
 {
-    public readonly ManagerResponseState State { get; }
-    public readonly T1? Result1 { get; }
-    public readonly T2? Result2 { get; }
+    public readonly ManagerResponseState State;
+    public readonly T1? Result1;
+    public readonly T2? Result2;
+
+    public ManagerResponse()
+    {
+        State = ManagerResponseState.Unknown;
+    }
 
     private ManagerResponse(ManagerResponseState state, T1? result1 = default, T2? result2 = default)
     {
